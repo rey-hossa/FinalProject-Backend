dbe872103a6a9113282085366db81ebd2030d050
rey-hossa <rey.hossa.rh@gmail.com>
2022-08-18 13:18:52 +0200

    add schema in provider

--- database/migrations/2022_08_01_202314_create_orders_table.php
+++ database/migrations/2022_08_01_202314_create_orders_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->integer('user_id');
             $table->integer('product_id');
             $table->integer('quantity');
+            $table->string('order_date');
             $table->timestamps();
         });
     }
